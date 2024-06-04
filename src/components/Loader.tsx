dae1fdd88cd2686d9d4fd9b808aaffe141162b08
“Lukasz <lukasz.glowacz@glowacz.se>
2024-06-04 19:06:05 +0200

    Margin-top for loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Loader: React.FC = () => {
   return (
-    <div className="d-flex vh-100 justify-content-center align-items-center mb-5">
+    <div className="d-flex vh-100 justify-content-center align-items-center mt-5">
       <Spinner animation="border" variant="info"/>
     </div>
   );
