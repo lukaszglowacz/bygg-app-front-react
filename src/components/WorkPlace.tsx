bb33690a39df7115809c7d086ed41d5760f734ee
“Lukasz <lukasz.glowacz@glowacz.se>
2024-06-07 18:02:39 +0200

    Added modal and refresh state to delete workplace data container

--- src/components/WorkPlace.tsx
+++ src/components/WorkPlace.tsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { useWorkPlaceData } from "../hooks/useWorkplaceData";
 import { Container, Col, Row, Button, Accordion } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { useUserProfile } from "../context/UserProfileContext";
 import Loader from "./Loader";
-import { PencilSquare, PlusSquare } from "react-bootstrap-icons";
+import { PencilSquare, PlusSquare, Trash } from "react-bootstrap-icons";
+import ConfirmModal from "./ConfirmModal";
+import api from "../api/api";
+import { IWorkPlacesData } from "../api/interfaces/types";
 
 const WorkPlaceContainer: React.FC = () => {
-  const workplaces = useWorkPlaceData();
+  const [workplaces, setWorkplaces] = useState<IWorkPlacesData[]>([]);
   const navigate = useNavigate();
   const { profile, loadProfile } = useUserProfile();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [workplaceToDelete, setWorkplaceToDelete] = useState<number | null>(null);
 
   useEffect(() => {
     const fetchProfile = async () => {
@@ -23,12 +27,47 @@ const WorkPlaceContainer: React.FC = () => {
     fetchProfile();
   }, [profile, loadProfile]);
 
+  const fetchWorkplaces = async () => {
+    try {
+      setLoading(true);
+      const response = await api.get<IWorkPlacesData[]>('/workplace/');
+      setWorkplaces(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Failed to fetch workplaces", error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchWorkplaces();
+  }, []);
+
   const handleAddClick = () => {
     navigate("/add-work-place");
   };
 
   const handleEditClick = (id: number) => {
     navigate(`/edit-work-place/${id}`);
+  };
+
+  const handleDeleteClick = (id: number) => {
+    setShowModal(true);
+    setWorkplaceToDelete(id);
+  };
+
+  const confirmDeleteWorkplace = async () => {
+    if (workplaceToDelete !== null) {
+      try {
+        await api.delete(`/workplace/${workplaceToDelete}/`);
+        setWorkplaceToDelete(null);
+        setShowModal(false);
+        fetchWorkplaces(); // Refresh the workplaces after deletion
+      } catch (error) {
+        console.error("Failed to delete the workplace", error);
+        setShowModal(false);
+      }
+    }
   };
 
   if (loading) {
@@ -62,7 +101,7 @@ const WorkPlaceContainer: React.FC = () => {
                 <Accordion.Header className="text-center">
                   <div className="d-flex flex-column justify-content-center">
                     <span>{`${workplace.street} ${workplace.street_number}`}</span>
-                    <span>{` ${workplace.postal_code} ${workplace.city}`}</span>
+                    <span>{`${workplace.postal_code} ${workplace.city}`}</span>
                   </div>
                 </Accordion.Header>
 
@@ -80,6 +119,17 @@ const WorkPlaceContainer: React.FC = () => {
                         </Button>
                         <div>Edit</div>
                       </div>
+                      <div className="text-center">
+                        <Button
+                          variant="danger"
+                          className="btn-sm p-0"
+                          onClick={() => handleDeleteClick(workplace.id)}
+                          title="Delete"
+                        >
+                          <Trash size={24} />
+                        </Button>
+                        <div>Delete</div>
+                      </div>
                     </div>
                   </Accordion.Body>
                 )}
@@ -88,6 +138,14 @@ const WorkPlaceContainer: React.FC = () => {
           </Accordion>
         </Col>
       </Row>
+
+      <ConfirmModal
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        onConfirm={confirmDeleteWorkplace}
+      >
+        Are you sure you want to delete this workplace?
+      </ConfirmModal>
     </Container>
   );
 };
